627b49f5364f47f1141b1a7e8481348e51e3a2f6
Raj Vasikarla <raj_vasikarla@intuit.com>
2018-10-08 14:09:14 -0700

    commit message

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var app = express();
 const hbs = require('hbs');
 
 app.use(express.static(__dirname + '/public'));
+hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
 
 app.get('/', (req, res) => {
